9e5a4c4ababa8a58b85d409a6f4d0b2c93e36f9c
Myles Wirth <regime_skylark.0y@icloud.com>
2025-09-22 17:18:15 -0400

    removed function to extract an urn id (irrelevant)

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,20 @@
 use rand::Rng;
 use tokio::time::{sleep, Duration};
 
+use crate::UniformResourceName;
+
 /// Evade detection by sleeping for a random duration.
 pub async fn evade() {
     let delay = rand::thread_rng().gen_range(2..=5);
     sleep(Duration::from_secs(delay)).await;
 }
 
-/// Extract ID from LinkedIn URN.
-pub fn get_id_from_urn(urn: &str) -> &str {
-    urn.split(':').nth(3).unwrap_or("")
+/// Extract ID
+pub(crate) fn get_id_from_urn(urn: Option<UniformResourceName>) -> Option<String> {
+    if let Some(secure_urn) = urn {
+        return Some(secure_urn.id)
+    } else {
+        return None
+    }
 }
+
